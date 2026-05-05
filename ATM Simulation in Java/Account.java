56c2b5ae54d9f90d7a172b06ca1ab55e778c25a6
Abdul Hanan <abdul.hanan.31372@gmail.com>
2026-05-05 05:46:10 +0500

    Refactor Account class and improve user interaction
    Refactor Account class to include pincode and improve deposit/withdraw methods. Enhance input validation and account interaction in main method.

--- ATM Simulation in Java/Account.java
+++ ATM Simulation in Java/Account.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class Account {
     String owner_name;
-    int acc_num;
+    int acc_num, pincode;
     double balance;
-    int pincode;
 
     public Account(int acc_num, String owner_name, double balance, int pincode) {
         this.acc_num = acc_num;
@@ -19,104 +18,132 @@ public class Account {
 
         if (amount <= 10000) {
             balance = balance - amount;
-        }
-
-        else if (amount > balance) {
+        } else if (amount > balance) {
             System.out.println("Insufficient Funds.");
-        }
-
-        else {
+        } else {
             System.out.println("Withdraw amount exceeds limit.");
         }
 
         System.out.println("Remaining balance = " + balance);
+        return;
     }
 
     void deposit(double amount) {
-        balance = balance + amount;
+        if (amount <= 0) {
+            System.out.println("Amount must be greater than zero.");
+        } else {
+            balance = balance + amount;
+            System.out.println("New balance = " + balance);
+        }
+        System.out.println("New balance = " + balance);
     }
 
+    public static void main(String[] args) {
+        int number = 0;
 
-    static void main(String[] args) {
-        Account acc = new Account();
         Data da = new Data();
-        int pin = da.pincode;
-
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter your account number : ");
 
+
         try {
-            int number = s.nextInt();
-        }
-        catch (Exception e) {
-            System.out.println();
+            number = s.nextInt();
+        } catch (Exception e) {
+            System.out.println("");
             System.out.println("Please enter a valid account number.");
 
-        }
-
-        int i = 3;
-
-        int code = 0;
-        for (i >= 0) {
-            do {
-                System.out.print("Please enter your 4 digit pin: ");
-
-                try {
-                    code = s.nextInt();
-                } catch (Exception e) {
-                    System.out.println("Given pin code is not valid");
-                }
-
-                if (code != pin) {
-                    System.out.println("Wrong pin code.");
-                }
-            } while (code != pin);
+        } finally {
 
 
-            System.out.println("Please select an option");
-            System.out.println("1. Deposit Cash");
-            System.out.println("2. Withdraw Cash");
-            System.out.println("3. Check Account Details");
-            System.out.println("4. Exit Simulation");
-            int option = 0;
-            double amount = 0;
+            if (number != da.acc_num) {
+                do {
 
-            if (option == 1) {
+                    try {
+                        number = s.nextInt();
+                    } catch (Exception e) {
+                        System.out.println("Please enter a valid account number.");
 
-                System.out.println("Enter amount you want to deposit: ");
-
-                try {
-                    amount = s.nextDouble();
-                } catch (Exception e) {
-                    System.out.println("Pleased enter a valid amount");
-                }
-
-                acc.deposit(amount);
-
-            } else if (option == 2) {
-
-                System.out.println("Enter amount you want to withdraw");
-
-                try {
-                    amount = s.nextDouble();
-                } catch (Exception e) {
-                    System.out.println("Please enter a valid amount");
-                }
-
-                acc.withdraw(amount);
-            } else if (option == 3) {
-
-                System.out.println("---- Account Details ----");
-                da.Printing();
-            } else if (option == 4) {
-
-                System.out.println("----- Exiting Simulation ------");
+                    }
+                } while (number != da.acc_num);
             } else {
-                System.out.println("Please enter a valid option.");
-            }
+                Account selectedAcc = da.gettingAcc(number);
 
+                int i = 3;
+                int code = 0;
+
+                while (i > 0 || code != selectedAcc.pincode) {
+                    System.out.print("Please enter your 4 digit pin: ");
+
+                    try {
+                        code = s.nextInt();
+                    } catch (Exception e) {
+                        System.out.println("Given pin code is not valid");
+                    }
+
+                    if (code != selectedAcc.pincode) {
+                        System.out.println("Wrong pin code.");
+                    } else if (code == selectedAcc.pincode) {
+                        break;
+                    }
+                    i = i - 1;
+                }
+                ;
+
+
+                int option = 0;
+                double amount = 0;
+
+                while (option != 4) {
+
+                    System.out.println("Please select an option");
+                    System.out.println("1. Deposit Cash");
+                    System.out.println("2. Withdraw Cash");
+                    System.out.println("3. Check Account Details");
+                    System.out.println("4. Exit Simulation");
+
+                    try {
+                        option = s.nextInt();
+                    } catch (Exception e) {
+                        System.out.println("Please enter a valid option.");
+                    }
+
+                    if (option == 1) {
+
+                        System.out.println("Enter amount you want to deposit: ");
+
+                        try {
+                            amount = s.nextDouble();
+                        } catch (Exception e) {
+                            System.out.println("Pleased enter a valid amount");
+                        }
+
+                        selectedAcc.deposit(amount);
+
+                    } else if (option == 2) {
+
+                        System.out.println("Enter amount you want to withdraw");
+
+                        try {
+                            amount = s.nextDouble();
+                        } catch (Exception e) {
+                            System.out.println("Please enter a valid amount");
+                        }
+
+                        selectedAcc.withdraw(amount);
+                    } else if (option == 3) {
+
+                        System.out.println("---- Account Details ----");
+                        da.Printing();
+                    } else if (option == 4) {
+
+                        System.out.println("----- Exiting Simulation ------");
+                    } else {
+                        System.out.println("Please enter a valid option.");
+                    }
+
+                }
+            }
         }
     }
 }
-
